d27b61e569c1373a97acbcf54635b0c0e44ab0d0
Daryl Moulder <dmoulder@qtome.com>
2014-05-21 17:52:24 +1000

    Update spec test and readme

--- spec/lib/mailgun/deliverer_spec.rb
+++ spec/lib/mailgun/deliverer_spec.rb
@@ -22,6 +22,10 @@ describe Mailgun::Deliverer do
       check_mailgun_message message_with_mailgun_variables, basic_expected_mailgun_message.merge('v:foo' => 'bar')
     end
 
+    it 'should invoke mailgun message transforming the mail gun operations' do
+      check_mailgun_message message_with_mailgun_operations, basic_expected_mailgun_message.merge('o:tag' => 'bar')
+    end
+    
     it 'should invoke mailgun message transforming the custom headers' do
       check_mailgun_message message_with_custom_headers, basic_expected_mailgun_message.merge('h:foo' => 'bar')
     end
@@ -83,6 +87,12 @@ describe Mailgun::Deliverer do
     def message_with_mailgun_variables
       message = basic_multipart_rails_message
       message.mailgun_variables = {foo: 'bar'}
+      message
+    end
+    
+    def message_with_mailgun_operations
+      message = basic_multipart_rails_message
+      message.mailgun_operations = {tag: 'bar'}
       message
     end
 
