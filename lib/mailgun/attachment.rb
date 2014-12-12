eea844d3bc2694289d710d8d053bf4d4be578836
Plehanov Dmitriy <onemanstartup@gmail.com>
2014-12-12 08:36:42 +0300

    attachments support(with inlines)

---
+++ lib/mailgun/attachment.rb
@@ -0,0 +1,18 @@
+module Mailgun
+  class Attachment < StringIO
+    attr_reader :original_filename, :content_type, :path
+
+    def initialize (attachment, *rest)
+      @path = ''
+      if rest.select {|opt| opt[:inline] }
+        basename = @original_filename = attachment.cid
+      else
+        basename = @original_filename = attachment.filename
+      end
+      @content_type = attachment.content_type.split(';')[0]
+      super attachment.body.decoded
+      binmode
+      rewind
+    end
+  end
+end
